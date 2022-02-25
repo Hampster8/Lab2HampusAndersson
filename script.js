9e4dab888787c41e03151a85bddea95b1f39088f
Hampus Andersson <70724079+Hampster8@users.noreply.github.com>
2022-02-25 09:24:24 +0100

    a

--- script.js
+++ script.js
@@ -4,3 +4,16 @@
 const validate = (name, surname, email, telephone) => {
     
 }
+
+
+function wordCount ({
+    const text = document.getElementById('messagebox').value;
+    let counter = 0;
+    text.forEach(letter => {
+        if (letter === " ") {
+            counter += 1
+        };
+    counter += 1; //To add the last word.
+    document.getElementById("counter").innerHTML = counter;
+}
+};
